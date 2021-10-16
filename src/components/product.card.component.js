ebe6441c63fe251d11b37c32ec8828d4d79e5526
Dheeraj <dheeraj.pant@testbook.com>
2021-10-16 13:28:42 +0530

    prettier applied

--- src/components/product.card.component.js
+++ src/components/product.card.component.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import AuthService from "../services/auth.service";
+import React, {Component} from "react";
 import {Button, Card, Col} from "react-bootstrap";
+import AuthService from "../services/auth.service";
+import {Link} from "react-router-dom";
 
 export default class ProductCardComponent extends Component {
     constructor(props) {
@@ -16,16 +17,16 @@ export default class ProductCardComponent extends Component {
     render() {
         const {product, onClick} = this.props
         return (
-                <Card style={{width: '18rem'}}>
-                    <Card.Img variant="top" src={product.image}/>
-                    <Card.Body>
-                        <Card.Title>{product.id}</Card.Title>
-                        <Card.Text>
-                            {product.name}
-                        </Card.Text>
-                        <Button variant="primary" onClick={this.onTrigger}>Buy Now</Button>
-                    </Card.Body>
-                </Card>
+            <Card style={{width: '18rem'}}>
+                <Card.Img variant="top" src={product.image}/>
+                <Card.Body>
+                    <Card.Title>{product.id}</Card.Title>
+                    <Card.Text>
+                        {product.name}
+                    </Card.Text>
+                    <Button variant="primary" onClick={this.onTrigger}>Details</Button>
+                </Card.Body>
+            </Card>
         );
     }
 }
